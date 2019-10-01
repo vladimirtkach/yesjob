85d3d9a5739c2bf15b66685d313b473b36f8cc66
Arthur <alteyka.da@gmail.com>
2019-10-01 23:07:37 +0300

    Написал пару вводных тестов.

--- src/employer/tests.py
+++ src/employer/tests.py
@@ -1,24 +1,51 @@
-from django.test import TestCase
-from .models import *
+import time
+
+from django.test import LiveServerTestCase
+from selenium import webdriver
 
 
-class EmployerTestCase(TestCase):
+class EmployerTestCase(LiveServerTestCase):
 
     def setUp(self):
-        create_vacancy = Vacancy.objects.create(position='sdev')
+        self.selenium = webdriver.Chrome()
+        super(EmployerTestCase, self).setUp()
 
-        Employer.objects.create(company_name='otus',
-                                country='UK',
-                                vacancy=create_vacancy)
+    def tearDown(self):
+        self.selenium.quit()
+        super(EmployerTestCase, self).tearDown()
 
-        Employer.objects.create(company_name='uber',
-                                country='USA',
-                                vacancy=create_vacancy)
+    def test_login_admin(self):
+        try:
+            selenium = self.selenium
+            selenium.get('http://127.0.0.1:8000/accounts/login/')
+
+            input_email = selenium.find_element_by_id('id_username')
+            input_email.send_keys('arthur-admin@gmail.com')
+
+            input_password = selenium.find_element_by_id('id_password')
+            input_password.send_keys('arthur-admin-password')
+
+            login_click = selenium.find_element_by_id('submit-id-sign_in')
+            login_click.click()
+
+            find_admin = selenium.find_element_by_xpath('//li[contains(text(), "admin")]')
+            assert find_admin != True
+        finally:
+            selenium.quit()
 
     def test_employer(self):
-        otus = Employer.objects.get(,,
-        uber = Employer.objects.get(,,
-        self.assertEqual(otus.country, "UK")
-        self.assertEqual(uber.country, "USA")
-        self.assertEqual(otus.vacancy, "sdev")
-        self.assertEqual(uber.vacancy, "sdev")
+        try:
+            selenium = self.selenium
+            selenium.get('http://127.0.0.1:8000/employer/')
+            find_link = selenium.find_element_by_class_name('container-fluid a')
+            find_link.click()
+            find_text = selenium.find_element_by_tag_name('h2')
+            assert "Please Log In" != find_text
+            time.sleep(5)
+        finally:
+            selenium.quit()
+
+
+
+
+
